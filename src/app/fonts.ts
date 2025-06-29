1781ca35795b40d02860dba9712dfa78cb6039ba
dxkrn <dxkrnn@gmail.com>
2025-06-29 12:47:45 +0700

    style: add color style

---
+++ src/app/fonts.ts
@@ -0,0 +1,10 @@
+import { Bricolage_Grotesque, Plus_Jakarta_Sans, Poppins } from 'next/font/google';
+
+export const bricolageGrotesque = Bricolage_Grotesque({
+    subsets: ['latin']
+});
+
+export const poppins = Poppins({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
