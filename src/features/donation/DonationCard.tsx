8580ca5c6aadf998e2e4f97e4df463c610716c55
dxkrn <dxkrnn@gmail.com>
2025-07-07 14:37:08 +0700

    feat: add image on donation card

--- src/features/donation/DonationCard.tsx
+++ src/features/donation/DonationCard.tsx
@@ -1,14 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function DoantionCard() {
     return (
         <Link href={''}>
-            <div className="border border-primary-light bg-neutral-0 p-2 rounded-lg">
-                <h5 className="text-base font-bold">Campaign for Clean Water Access</h5>
-                <p className="text-xs text-neutral-600 pt-2 truncate">Since 2010, AquaLife has been committed to providing clean water solutions in drought-stricken regions.</p>
+            <div className="border border-primary-light bg-neutral-0 p-4 rounded-lg">
+                <Image
+                    src={'/assets/images/donation-card-1.png'}
+                    width={864}
+                    height={576}
+                    alt=""
+                    className="rounded-sm"
+                />
+                <h5 className="text-base font-bold mt-4 md:text-lg lg:text-xl">Campaign for Clean Water Access</h5>
+                <p className="text-xs text-neutral-600 pt-2 truncate md:text-base lg:text-lg">Since 2010, AquaLife has been committed to providing clean water solutions in drought-stricken regions.</p>
                 <div className="flex gap-1 pt-4">
-                    <p className="text-sm font-semibold">$75,000</p>
-                    <p className="text-sm">raised of $150,000 goal</p>
+                    <p className="text-xs font-semibold md:text-sm lg:text-base">$75,000</p>
+                    <p className="text-xs md:text-sm lg:text-base">raised of $150,000 goal</p>
                 </div>
                 <div className="w-full h-2 bg-primary-thin rounded-full overflow-hidden mt-2">
                     <div
@@ -22,9 +30,9 @@ export default function DoantionCard() {
                             <path opacity="0.12" d="M16.1111 3C19.6333 3 22 6.3525 22 9.48C22 15.8138 12.1778 21 12 21C11.8222 21 2 15.8138 2 9.48C2 6.3525 4.36667 3 7.88889 3C9.91111 3 11.2333 4.02375 12 4.92375C12.7667 4.02375 14.0889 3 16.1111 3Z" fill="#B6A5DA" />
                             <path d="M16.1111 3C19.6333 3 22 6.3525 22 9.48C22 15.8138 12.1778 21 12 21C11.8222 21 2 15.8138 2 9.48C2 6.3525 4.36667 3 7.88889 3C9.91111 3 11.2333 4.02375 12 4.92375C12.7667 4.02375 14.0889 3 16.1111 3Z" stroke="#B6A5DA" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
-                        <p className="text-xs">5,200 supporters</p>
+                        <p className="text-xs md:text-sm lg:text-base">5,200 supporters</p>
                     </div>
-                    <p className="text-sm px-3 py-1 bg-neutral-200 rounded-full text-center">HEALTH</p>
+                    <p className="text-xs px-3 py-1 bg-neutral-200 rounded-full text-center md:text-sm lg:text-base">HEALTH</p>
                 </div>
             </div>
         </Link>
--- src/features/donation/DonationList.tsx
+++ src/features/donation/DonationList.tsx
@@ -2,7 +2,7 @@ import DoantionCard from "./DonationCard";
 
 export default function DonationList() {
     return (
-        <div className="grid grid-cols-1 gap-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             <DoantionCard />
             <DoantionCard />
             <DoantionCard />
